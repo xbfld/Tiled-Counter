9b828828596069fef46d4f08f6b9db489ef4b336
cehigi <xbfld0614@gmail.com>
2020-06-29 18:36:51 +0900

    Add counting part

--- script.js
+++ script.js
@@ -28,14 +28,15 @@ class GridBox {
 }
 
 class Tile extends GridBox {
-  constructor(i, j, infotext = `${i}.${j}`) {
+  constructor(i, j, infotext = `${i}.${j}`, tiletext = '@') {
     super();
     this.i = i;
     this.j = j;
     this.infotext = infotext;
+    this.tiletext = tiletext;
   }
-  getTopleft(){
-    return super.getTopleft([this.i,this.j])
+  getTopleft() {
+    return super.getTopleft([this.i, this.j])
   }
   getCenter() {
     let tileSize = [tilesize.w, tilesize.h];
@@ -57,7 +58,7 @@ class Tile extends GridBox {
     ctx.font = "30px D2Coding";
     ctx.textAlign = "center";
     ctx.textBaseline = "middle";
-    ctx.fillText("CD", ...this.getCenter());
+    ctx.fillText(this.tiletext, ...this.getCenter());
   }
 }
 
@@ -78,8 +79,8 @@ class Cursor extends GridBox {
     this.j = j;
     this.strokeStyle = strokeStyle;
   }
-  getTopleft(){
-    return super.getTopleft([this.i,this.j])
+  getTopleft() {
+    return super.getTopleft([this.i, this.j])
   }
   draw() {
     let margin = 15;
@@ -121,6 +122,40 @@ function update() {
   draw();
 }
 
+function renderRomanNumeral(n) {
+  const units = ['', 'I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX']
+  const tens = ['', 'X', 'XX', 'XXX', 'XL', 'L', 'LX', 'LXX', 'LXXX', 'XC']
+  const hundreds = ['', 'C', 'CC', 'CCC', 'CD', 'D', 'DC', 'DCC', 'DCCC', 'CM']
+  const thousands = ['', 'M', 'MM', 'MMM']
+  let digit = [...('' + n)]
+  let result = ''
+
+  result = units[digit.pop()] + result
+  if (digit.length == 0) { return result }
+  result = tens[digit.pop()] + result
+  if (digit.length == 0) { return result }
+  result = hundreds[digit.pop()] + result
+  if (digit.length == 0) { return result }
+  result = thousands[digit.pop()] + result
+
+  return result
+}
+
+function* getNextCount() {
+  let now = 1;
+  let separator = '□'
+  while (1) {
+    yield renderRomanNumeral(now) + separator
+    now += 1
+  }
+}
+var counter = getNextCount()
+var level = ['Start!',counter.next().value,counter.next().value]
+var state = { mylevel: 1, step: 0 }
+var stateLog = [state]
+
+const progression = document.getElementById('progression');
+
 var controller = {};
 controller.stepRight = function () {
   cursor.i += 1;
@@ -135,6 +170,38 @@ controller.stepDown = function () {
   cursor.j += 1;
 };
 controller.stepX = function () { };
+
+function renderProgress() {
+  let mylevel = state.mylevel
+  let pastText = level[mylevel-1]
+  let levelText = level[mylevel]
+  let nextText = level[mylevel+1]
+  progression.innerHTML =''
+  +`<div class='past'>Last Level: ${pastText}<br></div>`
+    +`<div class='passed'>Pass: ${levelText.slice(0, state.step)}<br></div>`
+    + `<div class='now'>Left: ${levelText.slice(state.step)}<br></div>`
+    + `<div class='next'>Next Level: ${nextText}</div>`
+}
+
+controller.nextStep = function () {
+  state.step += 1
+
+  // move on next level
+  if (state.step == level[state.mylevel].length) {
+    state.step = 0
+    state.mylevel += 1
+    level.push(counter.next().value)
+  }
+
+  // // prepare next level if mylevel exceeded
+  // if (state.mylevel == level.length) {
+  //   level.push(counter.next().value)
+  // }
+  renderProgress()
+}
+controller.rollBack = function () {
+
+}
 
 window.setInterval(update, 50);
 
@@ -152,7 +219,11 @@ function keydownHandler(e) {
     case "ArrowDown":
       controller.stepDown();
       break;
+    case "Space":
+      controller.nextStep();
+      break;
     case "KeyZ":
+      controller.rollBack();
       break;
     default:
       break;
@@ -160,3 +231,5 @@ function keydownHandler(e) {
 }
 
 window.addEventListener("keydown", keydownHandler);
+
+renderProgress();
